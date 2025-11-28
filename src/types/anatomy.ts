2599caae14cb81819483c3b4e35d95258092d044
Grant Cox <42614867+gcox32@users.noreply.github.com>
2025-11-28 18:46:20 -0500

    Add types

--- src/types/anatomy.ts
+++ src/types/anatomy.ts
@@ -1,18 +1,85 @@
+import { User } from "./user";
+
+type MuscleGroupName =
+    'chest' | 'anterior delts' | 'lateral delts' | 'posterior delts' |
+    'lats' | 'rhomboids' | 'traps' | 'spinal erectors' | 'quadratus lumborum' |
+    'biceps' | 'triceps' | 'forearms' |
+    'anterior core' | 'lateral core' | 'deep core' |
+    'glutes' | 'hip flexors' | 'quadriceps' | 'hamstrings' | 'adductors' | 'abductors' | 'calves' | 'anterior lower leg' |
+    'neck' | 'jaw' | 'other';
+
+
+type MuscleName =
+    // Upper Body
+    'pectoralis major' |
+    'anterior deltoid' | 'lateral deltoid' | 'posterior deltoid' |
+    'latissimus dorsi' | 'trapezius' | 'rhomboids' | 'teres major' |
+    'biceps brachii' | 'brachialis' | 'triceps brachii' | 'forearms' |
+
+    // Core
+    'rectus abdominis' |
+    'obliques' |
+    'transversus abdominis' |
+    'erector spinae' | 'quadratus lumborum' | 'multifidus' |
+    'serratus anterior' | 'iliopsoas' |
+
+    // Lower Body
+    'gluteus maximus' | 'gluteus medius' | 'gluteus minimus' |
+    'hamstrings' |
+    'rectus femoris' | 'vastus lateralis' | 'vastus medialis' | 'vastus intermedius' |
+    'adductor magnus' | 'adductors' |
+    'gastrocnemius' | 'soleus' |
+
+    'other';
+
+type ScientificName =
+    'soleus' | 'gastrocnemius' | 'tibialis anterior' | 'tibialis posterior' |
+    'biceps brachii' | 'triceps brachii' | 'brachialis' | 'brachioradialis' |
+    'trapezius' | 'levator scapulae' | 'erector spinae' | 'quadratus lumborum' |
+    'multifidus' | 'longissimus dorsi' | 'iliopsoas' | 'piriformis' |
+    'obliquus externus abdominis' | 'obliquus internus abdominis' | 'rectus abdominis' |
+    'transversus abdominis' | 'serratus anterior' | 'intercostals' | 'diaphragm' |
+    'sternocleidomastoid' | 'scalenes' |
+    'splenius capitis' | 'splenius cervicis' |
+    'pectoralis major' | 'pectoralis minor' |
+    'anterior deltoid' | 'lateral deltoid' | 'posterior deltoid' |
+    'latissimus dorsi' | 'rhomboid major' | 'rhomboid minor' |
+    'teres major' | 'teres minor' |
+    'infraspinatus' | 'supraspinatus' |
+    'pronator teres' | 'supinator' |
+    'flexor carpi radialis' | 'flexor carpi ulnaris' |
+    'extensor carpi radialis longus' | 'extensor carpi radialis brevis' |
+    'extensor carpi ulnaris' |
+    'gluteus maximus' | 'gluteus medius' | 'gluteus minimus' |
+    'tensor fasciae latae' |
+    'adductor magnus' | 'adductor longus' | 'adductor brevis' |
+    'pectineus' | 'gracilis' |
+    'biceps femoris long head' | 'biceps femoris short head' |
+    'semitendinosus' | 'semimembranosus' |
+    'rectus femoris' |
+    'vastus lateralis' | 'vastus medialis' | 'vastus intermedius' |
+    'flexor hallucis longus' | 'flexor digitorum longus' |
+    'extensor hallucis longus' | 'extensor digitorum longus' |
+    'peroneus longus' | 'peroneus brevis' |
+    'other';
+
+
 export interface MuscleGroup {
-    name: string;
+    id: string;
+    name: MuscleGroupName;
     description: string;
     muscles: Muscle[]; // hydrated with muscle data
 }
 
 export interface Muscle {
-    name: string;
-    description: string;
-    group: MuscleGroup; // hydrated with muscle group data
+    id: string;
+    name: MuscleName;
+    description?: string;
+    muscleGroupId: MuscleGroup['id'];
 }
 
 interface Measurement {
-    name: string;
-    description: string;
+    id: string;
     value: number;
     unit: string;
     method?: string;
@@ -20,16 +87,20 @@ interface Measurement {
 }
 
 interface MeasurementSite {
+    id: string;
     name: string;
-    description: string;
-    measurements: Measurement[]; // hydrated with measurement data
+    description?: string;
 }
 
 interface MeasurementInstance {
-    measurement: Measurement; // hydrated with measurement data
+    id: string;
+    userId: User['id'];
+    measurementId: Measurement['id'];
     date: Date;
 }
 
 export interface MeasurementLog {
+    id: string;
+    userId: User['id'];
     measurements: MeasurementInstance[]; // hydrated with measurement data
 }
--- src/types/train.ts
+++ src/types/train.ts
@@ -1,24 +1,164 @@
 import { MuscleGroup } from "./anatomy";
+import { User } from "./user";
 
-type ContractionType = 'isometric' | 'concentric' | 'eccentric';
+// helpers
+type PlaneOfMotion = 'sagittal' | 'frontal' | 'transverse';
+
+type Equipment = 'barbell' | 'dumbbell' | 'kettlebell' | 'machine' | 'bodyweight' | 'other';
+
+type Difficulty = 'beginner' | 'intermediate' | 'advanced';
+
+type RPE = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+type RestTimer = 0 | 15 | 30 | 45 | 60 | 90 | 120 | 180 | 240 | 300;
+
+type WorkoutType = 'strength' | 'hypertrophy' | 'endurance' | 'power' | 'skill' | 'other';
+
+type WorkoutBlockType = 'warm-up' | 'main' | 'accessory' | 'finisher' | 'other'; 
+
+type DistanceMeasurement = { value: number; unit: 'meters' | 'feet' | 'yards' | 'miles' };
+type TimeMeasurement     = { value: number; unit: 'seconds' | 'minutes' | 'hours' };
+type WeightMeasurement   = { value: number; unit: 'kg' | 'lbs' };
+type CaloriesMeasurement = { value: number; unit: 'calories' };
+type PaceMeasurement     = { value: number; unit: 'mph' | 'kph' | 'min/km' | 'min/mile' };
+type LongTimeMeasurement = { value: number; unit: 'days' | 'weeks' | 'months' | 'years' };
 
 interface MovementPattern {
     name: string;
     description: string;
 }
 
-export interface Exercise {
-    name: string;
-    description: string;
-    muscleGroups: MuscleGroup[];
-    movementPatterns: MovementPattern[];
+interface ExerciseMeasures {
+    externalLoad?:      WeightMeasurement;
+    includeBodyweight?: boolean;
+    reps?:              number;
+    distance?:          DistanceMeasurement;
+    time?:              TimeMeasurement;
+    pace?:              PaceMeasurement;
+    calories?:          CaloriesMeasurement;
 }
 
-export interface Set {
+
+// primary types
+interface Protocol {
+    id: string;
+    name: string;
+    description?: string;
+    workouts?: Workout[];
+    duration: LongTimeMeasurement;
+    daysPerWeek: number;
+    sessionsPerDay: number;
+    notes?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface Workout {
+    id: string;
+    userId: User['id'];
+    workoutType: WorkoutType;
+    name?: string;
+    blocks: WorkoutBlock[];
+    description?: string;
+    estimatedDuration?: number; // in minutes
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface WorkoutBlock {
+    id: string;
+    workoutId: Workout['id'];
+    workoutBlockType: WorkoutBlockType;
+    name?: string;
+    description?: string;
+    order: number;
+    exercises: WorkoutBlockExercise[];
+    circuit?: boolean;
+    estimatedDuration?: number; // in minutes
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface WorkoutBlockExercise {
+    id: string;
     exercise: Exercise;
-    reps: number;
-    weight: number;
-    rpe: number;
-    restTime: number;
-    notes: string;
+    order: number;
+    sets: number;
+    measures: ExerciseMeasures;
+    tempo?: {
+        eccentric:  TimeMeasurement;
+        bottom:     TimeMeasurement;
+        concentric: TimeMeasurement;
+        top:        TimeMeasurement;
+    };
+    restTime?: RestTimer;
+    rpe?: RPE;
+    notes?: string;
+}
+
+interface Exercise {
+    name: string;
+    description: string;
+    movementPattern: MovementPattern;
+    muscleGroups: {
+        primary: MuscleGroup;
+        secondary?: MuscleGroup;
+        tertiary?: MuscleGroup;
+    };
+    planeOfMotion?: PlaneOfMotion;
+    bilateral?: boolean;
+    equipment?: Equipment;
+    imageUrl?: string;
+    videoUrl?: string;
+    difficulty?: Difficulty;
+}
+
+// instances
+interface ProtocolInstance {
+    id: string;
+    userId: User['id'];
+    protocolId: Protocol['id'];
+    startDate: Date;
+    endDate?: Date | null;
+    complete: boolean;
+    duration?: number; // in minutes
+    notes?: string;
+
+}
+
+export interface WorkoutInstance {
+    id: string;
+    userId: User['id'];
+    workoutId: Workout['id'];
+    date: Date;
+    complete: boolean;
+    duration?: number; // in minutes
+    volume?: WeightMeasurement;
+    loadVolume?: WeightMeasurement;
+    notes?: string;
+}
+
+export interface WorkoutBlockInstance {
+    id: string;
+    userId: User['id'];
+    workoutInstanceId: WorkoutInstance['id'];
+    workoutBlockId: WorkoutBlock['id'];
+    date: Date;
+    complete: boolean;
+    duration?: number; // in minutes
+    volume?: WeightMeasurement;
+    loadVolume?: WeightMeasurement;
+    notes?: string;
+}
+
+export interface WorkoutBlockExerciseInstance {
+    id: string;
+    userId: User['id'];
+    workoutBlockInstanceId: WorkoutBlockInstance['id'];
+    workoutBlockExerciseId: WorkoutBlockExercise['id'];
+    date: Date;
+    complete: boolean;
+    duration?: number; // in minutes
+    measures: ExerciseMeasures;
+    notes?: string;
 }
